caef1e65bef884bbc14cf7317b7464896eea712e
Pamela Iglesias <pamela.fabia@blucher.com.br>
2022-06-26 22:07:58 -0300

    router na app + ajustes de index e web route

--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 <?php
+// Autoloader
+require_once '../vendor/autoload.php';
+
 // Load Config
 require_once '../config/config.php';
-require_once '../vendor/autoload.php';
+
+// Routes
+require_once '../routes/web.php';
+require_once '../app/Router.php';
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
 // Routes System
+$routes = new RouteCollection();
+$routes->add('turbine', new Route(constant('URL_SUBFOLDER') . '/turbine/{id}', array('controller' => 'TurbineController', 'method'=>'showTurbine'), array('id' => '[0-9]+')));
